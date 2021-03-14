d2c4151b23d5a2d7144355c378cb9b81f8571b01
Nicopicks <nicolas-picot@orange.fr>
2021-03-14 12:42:30 +0100

    implement base files and WebSockets management

--- pg_messenger/lib/main.dart
+++ pg_messenger/lib/main.dart
@@ -1,4 +1,11 @@
+import 'package:flutter/foundation.dart';
+import 'package:pg_messenger/Models/webSocketManager.dart';
+import 'package:pg_messenger/View/home.dart';
+import 'package:pg_messenger/View/login.dart';
+import 'package:pg_messenger/View/messageView.dart';
+import 'package:web_socket_channel/io.dart';
 import 'package:flutter/material.dart';
+import 'package:web_socket_channel/web_socket_channel.dart';
 
 void main() {
   runApp(MyApp());
@@ -8,37 +15,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Le nouveau messenger',
+      title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Le nouveau messenger'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[],
-        ),
-      ),
+      home: Login(),
     );
   }
 }
