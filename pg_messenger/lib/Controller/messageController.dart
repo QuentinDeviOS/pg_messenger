686f7732b95af8b232e12db44373eb9d1a173dfd
Nicopicks <nicolas-picot@orange.fr>
2021-03-18 14:04:49 +0100

    adding Stream for messageList

--- pg_messenger/lib/Controller/messageController.dart
+++ pg_messenger/lib/Controller/messageController.dart
@@ -1,3 +1,45 @@
+import 'dart:async';
+import 'dart:convert';
+
+import 'package:pg_messenger/Models/messages.dart';
+import 'package:web_socket_channel/io.dart';
+
 class MessageController {
-  var array = ["1", "2"];
+  StreamController<List<Message>> controller =
+      StreamController<List<Message>>();
+  List<Message> _messageList = [];
+  List<Message> get messageList {
+    return _messageList;
+  }
+
+  set messageList(List<Message> messageList) {
+    _messageList = messageList;
+    controller.add(messageList);
+  }
+
+  MessageController() {
+    controller.add(messageList);
+  }
+
+  void messageStream(Future<IOWebSocketChannel> futureChannel) async {
+    final channel = await futureChannel;
+    channel.stream.listen((message) {
+      hasMessage(message);
+      print("receive data");
+    });
+  }
+
+  bool hasMessage(dynamic messageReceived) {
+    final List<dynamic> messageListJson = jsonDecode(messageReceived);
+    if (messageListJson.isNotEmpty) {
+      messageList = [];
+      for (var messageJson in messageListJson) {
+        if (Message.fromJson(messageJson) != null) {
+          messageList.add(Message.fromJson(messageJson));
+          print("add message to messageList on controller");
+        }
+      }
+    }
+    return false;
+  }
 }
--- pg_messenger/lib/View/messageView.dart
+++ pg_messenger/lib/View/messageView.dart
@@ -1,5 +1,9 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:pg_messenger/Controller/WebSocketController.dart';
+import 'package:pg_messenger/Controller/messageController.dart';
+import 'package:pg_messenger/Models/messages.dart';
 
 class MessageView extends StatefulWidget {
   @override
@@ -8,14 +12,37 @@ class MessageView extends StatefulWidget {
 
 class _MessageViewState extends State<MessageView> {
   final webSocketController = WebSocketController();
+  final messageController = MessageController();
 
+  List<Message> get messageList {
+    return messageController.messageList;
+  }
+
+  _MessageViewState() {
+    messageController.messageStream(webSocketController.channel);
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
       body: Column(
-        children: [],
+        children: [
+          Expanded(
+            child: StreamBuilder(
+                stream: messageController.controller.stream,
+                builder: (context, snapshot) {
+                  return ListView.builder(
+                    itemBuilder: _listBuilder,
+                    itemCount: messageList.length,
+                  );
+                }),
+          ),
+        ],
       ),
     );
+  }
+
+  Widget _listBuilder(BuildContext context, int numberOfRow) {
+    return Text(messageList[numberOfRow].message);
   }
 }
