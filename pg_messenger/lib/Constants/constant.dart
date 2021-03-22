a6293db04aed8a7bc7521c566b4e979518ae1a7c
cedric06nice <cedric06nice@mac.com>
2021-03-22 11:52:38 +0000

    # Conflicts: #	pg_messenger/lib/Constants/constant.dart #	pg_messenger/lib/View/messageView.dart

--- pg_messenger/lib/Constants/constant.dart
+++ pg_messenger/lib/Constants/constant.dart
@@ -6,17 +6,15 @@ class Constant {
   //static const TEST_USER_TOKEN = "oPIbqQKdMI5F91OZaPi0Qw==";
 
   /// CEDRIC
-  static const URL_WEB_SERVER =
+  //static const URL_WEB_SERVER =
       "ws://localhost:8080/messages/message-web-socket";
-  static const TEST_USER_USERNAME = "qwerty";
-  static const TEST_USER_LOGIN_PASSWORD = "qwerty";
-  static const TEST_USER_ID = "0050608F-68BD-47FB-8049-C05716540BCF";
-  static const TEST_USER_TOKEN = "6pGhq3y1KVweB+SOSW0aMw==";
+  //static const TEST_USER_USERNAME = "qwerty";
+  //static const TEST_USER_ID = "0050608F-68BD-47FB-8049-C05716540BCF";
+  //static const TEST_USER_TOKEN = "6pGhq3y1KVweB+SOSW0aMw==";
 
   /// LIVE
-  //static const URL_WEB_SERVER = "wss://skyisthelimit.net:443/messages/message-web-socket";
-  //static const TEST_USER_USERNAME = "qwerty";
-  //static const TEST_USER_LOGIN_PASSWORD = "qwerty";
-  //static const TEST_USER_ID = "E8E80012-0D3F-4914-8685-4132A4B2BFA4";
-  //static const TEST_USER_TOKEN = "woe5f0zuvQKQgxrFerh19Q==";
+  static const URL_WEB_SERVER = "wss://skyisthelimit.net:443/messages/message-web-socket";
+  static const TEST_USER_USERNAME = "qwerty";
+  static const TEST_USER_ID = "E8E80012-0D3F-4914-8685-4132A4B2BFA4";
+  static const TEST_USER_TOKEN = "woe5f0zuvQKQgxrFerh19Q==";
 }
