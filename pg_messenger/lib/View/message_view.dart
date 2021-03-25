b09984f7cc87b276c5570d16e9e8a4285c6d4abf
Nicopicks <34891827+Nicopicks@users.noreply.github.com>
2021-03-25 13:11:02 +0100

    del useless import

--- pg_messenger/lib/View/message_view.dart
+++ pg_messenger/lib/View/message_view.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/scheduler.dart';
 import 'package:pg_messenger/Controller/message_controller.dart';
 import 'package:pg_messenger/Models/message.dart';
 import 'package:pg_messenger/Models/user.dart';
