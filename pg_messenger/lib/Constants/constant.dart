26ad3a65c950740173b89396e311f5d449dc172e
Nicopicks <34891827+Nicopicks@users.noreply.github.com>
2021-03-30 13:39:30 +0200

    add constant file

---
+++ pg_messenger/lib/Constants/constant.dart
@@ -0,0 +1,8 @@
+class Constant {
+  static const URL_WEB_SERVER = "wss://skyisthelimit.net/messages/message-web-socket";
+  static const JSONKEY_MESSAGE_USERNAME = "username";
+  static const JSONKEY_MESSAGE_TIMESTAMP = "timestamp";
+  static const JSONKEY_MESSAGE_ID = "id";
+  static const JSONKEY_MESSAGE_USERID = "userID";
+  static const JSONKEY_MESSAGE_MESSAGE = "subject";
+}
