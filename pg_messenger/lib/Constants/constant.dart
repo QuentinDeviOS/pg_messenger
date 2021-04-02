d810de83303dd244196c243dfe297947036c7739
Nicolas Picot <nicolas-picot@orange.fr>
2021-04-02 20:19:26 +0200

    Change constant file for test

--- pg_messenger/lib/Constants/constant.dart
+++ pg_messenger/lib/Constants/constant.dart
@@ -1,7 +1,19 @@
-class Constant {
+/* class Constant {
   static const URL_BASE = "skyisthelimit.net";
   static const URL_WEB_SERVER_BASE = "https://" + URL_BASE;
-  static const URL_WEB_SERVER = "wss://skyisthelimit.net/messages/message-web-socket";
+  static const URL_WEB_SOCKET = "wss://skyisthelimit.net/messages/message-web-socket";
+  static const JSONKEY_MESSAGE_USERNAME = "username";
+  static const JSONKEY_MESSAGE_TIMESTAMP = "timestamp";
+  static const JSONKEY_MESSAGE_ID = "id";
+  static const JSONKEY_MESSAGE_USERID = "userID";
+  static const JSONKEY_MESSAGE_MESSAGE = "message";
+} */
+
+//LOCAL TEST
+class Constant {
+  static const URL_BASE = "127.0.0.1";
+  static const URL_WEB_SERVER_BASE = "http://" + URL_BASE;
+  static const URL_WEB_SOCKET = "ws://127.0.0.1:8080/messages/message-web-socket";
   static const JSONKEY_MESSAGE_USERNAME = "username";
   static const JSONKEY_MESSAGE_TIMESTAMP = "timestamp";
   static const JSONKEY_MESSAGE_ID = "id";
