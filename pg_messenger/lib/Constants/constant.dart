25f334e8dc822a1faddcc68a2b7ffb6885c2d8a9
Nicopicks <34891827+Nicopicks@users.noreply.github.com>
2021-04-02 18:07:17 +0200

    refactor name Constant

--- pg_messenger/lib/Constants/constant.dart
+++ pg_messenger/lib/Constants/constant.dart
@@ -1,7 +1,7 @@
 class Constant {
   static const URL_BASE = "skyisthelimit.net";
   static const URL_WEB_SERVER_BASE = "https://" + URL_BASE;
-  static const URL_WEB_SERVER = "wss://skyisthelimit.net/messages/message-web-socket";
+  static const URL_WEB_SOCKET = "wss://skyisthelimit.net/messages/message-web-socket";
   static const JSONKEY_MESSAGE_USERNAME = "username";
   static const JSONKEY_MESSAGE_TIMESTAMP = "timestamp";
   static const JSONKEY_MESSAGE_ID = "id";
