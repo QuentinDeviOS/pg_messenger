9d0beceaffd5a99225ff04bfdf08e1e07902d8f9
Nicopicks <34891827+Nicopicks@users.noreply.github.com>
2021-04-14 11:28:49 +0200

    contant live

--- pg_messenger/lib/Constants/constant.dart
+++ pg_messenger/lib/Constants/constant.dart
@@ -1,14 +1,13 @@
 class Constant {
   //LOCAL
-  // static const URL_BASE = "192.168.1.25:8080";
-  // static const URL_WEB_SERVER_BASE = "http://" + URL_BASE;
-  // static const URL_WEB_SOCKET = "ws://192.168.1.25:8080/messages/message-web-socket";
+  static const URL_BASE = "192.168.1.25:8080";
+  static const URL_WEB_SERVER_BASE = "http://" + URL_BASE;
+  static const URL_WEB_SOCKET = "ws://192.168.1.25:8080/messages/message-web-socket";
 
   // LIVE
-  static const URL_BASE = "skyisthelimit.net";
-  static const URL_WEB_SERVER_BASE = "https://" + URL_BASE;
-  static const URL_WEB_SOCKET =
-      "wss://skyisthelimit.net/messages/message-web-socket";
+  // static const URL_BASE = "skyisthelimit.net";
+  // static const URL_WEB_SERVER_BASE = "https://" + URL_BASE;
+  // static const URL_WEB_SOCKET = "wss://skyisthelimit.net/messages/message-web-socket";
 
   static const PATH_TO_GET_PICTURE = "/photos/get-picture";
 
@@ -37,6 +36,7 @@ class Constant {
   static const JSONKEY_MESSAGE_OWNER_ID = "id";
   static const JSONKEY_MESSAGE_IS_PICTURE = "isPicture";
   static const JSONKEY_MESSAGE_CHANNEL = "channel";
+  static const JSONKEY_MESSAGE_OWNER_PICTURE = "owner_picture";
 
   static const JSONKEY_MESSAGE_USERNAME = "username";
   static const JSONKEY_MESSAGE_PASSWORD = "password";
