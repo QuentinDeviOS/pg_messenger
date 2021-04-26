0c370fb8d3366d7afd18f14934c1eae8da6ce4e4
cedric06nice <cedric06nice@mac.com>
2021-04-26 21:23:01 +0100

    Change password final

--- pg_messenger/lib/View/user_settings_view.dart
+++ pg_messenger/lib/View/user_settings_view.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart';
 import 'package:pg_messenger/main.dart';
 import 'package:http/http.dart' as http;
 import 'package:modal_bottom_sheet/modal_bottom_sheet.dart';
@@ -10,7 +11,6 @@ import 'package:pg_messenger/Models/user.dart';
 import 'package:pg_messenger/View/Connection/connection_view.dart';
 import 'package:pg_messenger/View/Connection/loading_view.dart';
 import 'package:pg_messenger/generated/l10n.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 
 class UserSettingsView extends StatefulWidget {
   final MessageController messageController;
@@ -71,115 +71,115 @@ class _UserSettingsViewState extends State<UserSettingsView> {
                   onPressed: () => onTapAddingPicture(context),
                 ),
               ),
-              Form(
-                child: Padding(
-                  padding: const EdgeInsets.fromLTRB(20, 30, 20, 0),
-                  child: Column(
-                    children: [
-                      Text(
-                        "Changer mon mot de passe :",
-                        style: TextStyle(
-                            fontSize: 15, fontWeight: FontWeight.w600),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: TextFormField(
-                          controller: _actualPasswordController,
-                          autofillHints: [AutofillHints.password],
-                          obscureText: true,
-                          autocorrect: false,
-                          decoration: InputDecoration(
-                              hintText: "Mot de passe actuelle"),
-                          onFieldSubmitted: (value) {
-                            if (_actualPasswordController.text == "") {
-                              FocusScope.of(context).unfocus();
-                            } else {
-                              _focusFirstNewPassword.requestFocus();
-                            }
-                          },
+              //if(_showChangePassword)
+              Container(
+                child: Form(
+                  child: Padding(
+                    padding: const EdgeInsets.fromLTRB(20, 30, 20, 0),
+                    child: Column(
+                      children: [
+                        Text(
+                          "Changer mon mot de passe :",
+                          style: TextStyle(
+                              fontSize: 15, fontWeight: FontWeight.w600),
                         ),
-                      ),
-                      Padding(padding: EdgeInsets.fromLTRB(0, 20, 0, 0)),
-                      Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: TextFormField(
-                          controller: _firstNewPassword,
-                          focusNode: _focusFirstNewPassword,
-                          autofillHints: [AutofillHints.password],
-                          obscureText: true,
-                          autocorrect: false,
-                          decoration:
-                              InputDecoration(hintText: "Nouveau mot de passe"),
-                          onFieldSubmitted: (value) {
-                            if (_actualPasswordController.text == "") {
-                              FocusScope.of(context).unfocus();
-                            } else {
-                              _focusFirstNewPassword.requestFocus();
-                            }
-                          },
-                        ),
-                      ),
-                      Padding(padding: EdgeInsets.fromLTRB(0, 20, 0, 0)),
-                      Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: TextFormField(
-                          controller: _secondNewPassword,
-                          focusNode: _focusSecondNewPassword,
-                          autofillHints: [AutofillHints.password],
-                          obscureText: true,
-                          autocorrect: false,
-                          decoration:
-                              InputDecoration(hintText: "Nouveau mot de passe"),
-                          onFieldSubmitted: (value) {
-                            if (_actualPasswordController.text == "") {
-                              FocusScope.of(context).unfocus();
-                            } else {
-                              _focusFirstNewPassword.requestFocus();
-                            }
-                          },
-                        ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.all(20.0),
-                        child: ElevatedButton(
-                            onPressed: () => _changePassword(context),
-                            child: Text("Changer mon mot de passe")),
-                      ),
-                      TextButton(
-                        child: Padding(
-                          padding: const EdgeInsets.fromLTRB(0, 0, 15, 15),
-                          child: Row(
-                            children: [
-                              Spacer(),
-                              Icon(
-                                Icons.logout,
-                                color: Theme.of(context)
-                                    .colorScheme
-                                    .textDarkModeTitle,
-                              ),
-                              Padding(padding: EdgeInsets.fromLTRB(8, 0, 0, 0)),
-                              Text(
-                                S.of(context).logout_title,
-                                style: TextStyle(
-                                    color: Theme.of(context)
-                                        .colorScheme
-                                        .textDarkModeTitle),
-                              )
-                            ],
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: TextFormField(
+                            controller: _actualPasswordController,
+                            autofillHints: [AutofillHints.password],
+                            obscureText: true,
+                            autocorrect: false,
+                            decoration: InputDecoration(
+                                hintText: "Mot de passe actuelle"),
+                            onFieldSubmitted: (value) {
+                              if (_actualPasswordController.text == "") {
+                                FocusScope.of(context).unfocus();
+                              } else {
+                                _focusFirstNewPassword.requestFocus();
+                              }
+                            },
                           ),
                         ),
-                        onPressed: () {
-                          _messageController.logOut();
-                          Navigator.pushReplacement(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) => ConnectionView()));
-                        },
+                        Padding(padding: EdgeInsets.fromLTRB(0, 20, 0, 0)),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: TextFormField(
+                            controller: _firstNewPassword,
+                            focusNode: _focusFirstNewPassword,
+                            autofillHints: [AutofillHints.newPassword],
+                            obscureText: true,
+                            autocorrect: false,
+                            decoration: InputDecoration(
+                                hintText: "Nouveau mot de passe"),
+                            onFieldSubmitted: (value) {
+                              if (_actualPasswordController.text == "") {
+                                FocusScope.of(context).unfocus();
+                              } else {
+                                _focusSecondNewPassword.requestFocus();
+                              }
+                            },
+                          ),
+                        ),
+                        Padding(padding: EdgeInsets.fromLTRB(0, 20, 0, 0)),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: TextFormField(
+                            controller: _secondNewPassword,
+                            focusNode: _focusSecondNewPassword,
+                            autofillHints: [AutofillHints.newPassword],
+                            obscureText: true,
+                            autocorrect: false,
+                            decoration: InputDecoration(
+                                hintText: "Nouveau mot de passe"),
+                            onFieldSubmitted: (value) {
+                              if (_actualPasswordController.text == "") {
+                                FocusScope.of(context).unfocus();
+                              }
+                            },
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(20.0),
+                          child: ElevatedButton(
+                              onPressed: () => _changePassword(context),
+                              child: Text("Changer mon mot de passe")),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+              TextButton(
+                child: Padding(
+                  padding: const EdgeInsets.fromLTRB(0, 0, 15, 15),
+                  child: Row(
+                    children: [
+                      Spacer(),
+                      Icon(
+                        Icons.logout,
+                        color: Theme.of(context).colorScheme.textDarkModeTitle,
                       ),
+                      Padding(padding: EdgeInsets.fromLTRB(8, 0, 0, 0)),
+                      Text(
+                        S.of(context).logout_title,
+                        style: TextStyle(
+                            color: Theme.of(context)
+                                .colorScheme
+                                .textDarkModeTitle),
+                      )
                     ],
                   ),
                 ),
-              )
+                onPressed: () {
+                  _messageController.logOut();
+                  Navigator.pushAndRemoveUntil(context,
+                      MaterialPageRoute(builder: (context) => ConnectionView()),
+                      (_) {
+                    return false;
+                  });
+                },
+              ),
             ],
           ),
         ),
@@ -257,39 +257,36 @@ class _UserSettingsViewState extends State<UserSettingsView> {
         });
   }
 
-  Future<http.Response> _updatePassword(String password, String newPassword,
-      {String? token}) {
-    String newPasswordToSend = "newPassword";
-    return http.post(
-      Uri.parse(Constant.URL_WEB_SERVER_BASE + '/users/update-password'),
-      headers: <String, String>{
-        'Content-Type': 'application/json; charset=UTF-8',
-        'Autorization': "Bearer $token"
-      },
-      body: jsonEncode(<String, dynamic>{
-        Constant.JSONKEY_USER_PASSWORD: password,
-        newPasswordToSend: newPassword,
-      }),
-    );
+  Future<Response> _updatePassword(
+      User user, String currentPassword, String newPassword) async {
+    Map<String, String> headers = Map();
+    headers["Authorization"] = "Bearer ${user.token}";
+    headers["Content-Type"] = "application/json; charset=utf-8";
+    var body = JsonEncoder().convert(
+        {"currentPassword": currentPassword, "newPassword": newPassword});
+    var response = await http.post(
+        Uri.parse(Constant.URL_WEB_SERVER_BASE + '/users/update-password'),
+        headers: headers,
+        body: body);
+    return response;
   }
 
   Future<User?> _changePassword(context) async {
-    String password = _passwordController.text;
-    String newPassword = _newPasswordController.text;
-    String newPasswordVerification = _newPasswordVerificationController.text;
+    String password = _actualPasswordController.text;
+    String newPassword = _firstNewPassword.text;
+    String newPasswordVerification = _secondNewPassword.text;
 
     if (newPassword != newPasswordVerification) {
       _wrongInput(context, S.of(context).register_error_password);
       return null;
     } else if (newPassword.isNotEmpty) {
-      final response = await _updatePassword(password, newPassword);
+      final response = await _updatePassword(
+          _messageController.currentUser, password, newPassword);
       if (response.statusCode == 200) {
-        User user = User.fromJsonResponseLogin(jsonDecode(response.body));
-        await _registerToken(user.token);
         await Navigator.push(
             context, MaterialPageRoute(builder: (context) => LoadingView()));
       } else {
-        _wrongUpdatePassword(context, response.body);
+        _wrongUpdatePassword(context, jsonDecode(response.body)["reason"]);
       }
       return null;
     }
@@ -313,8 +310,7 @@ class _UserSettingsViewState extends State<UserSettingsView> {
     );
   }
 
-  _wrongUpdatePassword(BuildContext context, String responseBodyError) {
-    Map<String, dynamic> json = jsonDecode(responseBodyError);
+  _wrongUpdatePassword(BuildContext context, String error) {
     Widget okButton = TextButton(
       child: Text(S.of(context).register_alert_OK_button),
       onPressed: () => Navigator.of(context).pop(),
@@ -322,7 +318,7 @@ class _UserSettingsViewState extends State<UserSettingsView> {
 
     AlertDialog alert = AlertDialog(
       title: Text(S.of(context).register_alert_title),
-      content: Text(json[Constant.JSONKEY_USER_RESPONSE_ERROR_REASON]),
+      content: Text(error),
       actions: [okButton],
     );
 
@@ -330,10 +326,5 @@ class _UserSettingsViewState extends State<UserSettingsView> {
       context: context,
       builder: (BuildContext context) => alert,
     );
-  }
-
-  _registerToken(String token) async {
-    final prefs = await SharedPreferences.getInstance();
-    prefs.setString(Constant.JSONKEY_TOKEN, token);
   }
 }
