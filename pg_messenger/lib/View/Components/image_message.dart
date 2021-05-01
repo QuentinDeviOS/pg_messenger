05fc4cb80d8e9573f04a33fa64a39b692a290b02
Nicolas Picot <nicolas-picot@orange.fr>
2021-05-02 01:16:01 +0200

    Modify card for image with elevation: 2, delete background color  and add padding inside the card

--- pg_messenger/lib/View/Components/image_message.dart
+++ pg_messenger/lib/View/Components/image_message.dart
@@ -3,30 +3,32 @@ import 'package:modal_bottom_sheet/modal_bottom_sheet.dart';
 import 'package:pg_messenger/Constants/constant.dart';
 import 'package:pg_messenger/Controller/message_controller.dart';
 import 'package:pg_messenger/Models/message.dart';
-import 'package:pg_messenger/main.dart';
 
 Widget imageMessage({required MessageController messageController, required Message message, required BuildContext context}) {
   Map<String, String> headers = Map();
   headers["Authorization"] = "Bearer ${messageController.currentUser.token}";
   return GestureDetector(
     child: Card(
-      color: Theme.of(context).colorScheme.bakgroundImageCard,
+      elevation: 2,
       child: Container(
         height: 200,
         width: 200,
-        child: Image.network(
-          Constant.URL_WEB_SERVER_BASE + Constant.PATH_TO_GET_PICTURE + "?filename=${message.message}",
-          loadingBuilder: (context, child, loadingProgress) {
-            if (loadingProgress == null) {
-              WidgetsBinding.instance?.addPostFrameCallback((timeStamp) {
-                messageController.jumpToEndAfterImageLoaded();
-              });
-              return child;
-            }
-            return CircularProgressIndicator();
-          },
-          headers: headers,
-          filterQuality: FilterQuality.low,
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Image.network(
+            Constant.URL_WEB_SERVER_BASE + Constant.PATH_TO_GET_PICTURE + "?filename=${message.message}",
+            loadingBuilder: (context, child, loadingProgress) {
+              if (loadingProgress == null) {
+                WidgetsBinding.instance?.addPostFrameCallback((timeStamp) {
+                  messageController.jumpToEndAfterImageLoaded();
+                });
+                return child;
+              }
+              return CircularProgressIndicator();
+            },
+            headers: headers,
+            filterQuality: FilterQuality.low,
+          ),
         ),
       ),
     ),
