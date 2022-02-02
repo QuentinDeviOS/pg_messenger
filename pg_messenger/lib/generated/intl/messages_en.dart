fab66a88b82904cb0277d150042de2b277c69cd5
Quentin Rolland <quentin.rolland@yahoo.fr>
2022-02-02 12:27:13 +0100

    V1.5.3 build 7

--- pg_messenger/lib/generated/intl/messages_en.dart
+++ pg_messenger/lib/generated/intl/messages_en.dart
@@ -7,7 +7,8 @@
 // ignore_for_file:unnecessary_brace_in_string_interps, unnecessary_new
 // ignore_for_file:prefer_single_quotes,comment_references, directives_ordering
 // ignore_for_file:annotate_overrides,prefer_generic_function_type_aliases
-// ignore_for_file:unused_import, file_names
+// ignore_for_file:unused_import, file_names, avoid_escaping_inner_quotes
+// ignore_for_file:unnecessary_string_interpolations, unnecessary_string_escapes
 
 import 'package:intl/intl.dart';
 import 'package:intl/message_lookup_by_library.dart';
@@ -19,53 +20,78 @@ typedef String MessageIfAbsent(String messageStr, List<dynamic> args);
 class MessageLookup extends MessageLookupByLibrary {
   String get localeName => 'en';
 
-  static m0(link) => "Could not launch ${link}";
+  static String m0(link) => "Could not launch ${link}";
 
   final messages = _notInlinedMessages(_notInlinedMessages);
-  static _notInlinedMessages(_) => <String, Function> {
-    "channel" : MessageLookupByLibrary.simpleMessage("Channels"),
-    "channel_new_button" : MessageLookupByLibrary.simpleMessage("     Create"),
-    "channel_new_checkbox" : MessageLookupByLibrary.simpleMessage("Public access"),
-    "channel_new_input_hint" : MessageLookupByLibrary.simpleMessage("Channel name"),
-    "channel_new_title" : MessageLookupByLibrary.simpleMessage("Create new channel"),
-    "login_alert_OK_button" : MessageLookupByLibrary.simpleMessage("OK"),
-    "login_error_text" : MessageLookupByLibrary.simpleMessage("Username and/or password do not match."),
-    "login_error_title" : MessageLookupByLibrary.simpleMessage("Are you sure about your credentials?"),
-    "login_password" : MessageLookupByLibrary.simpleMessage("password"),
-    "login_send_button" : MessageLookupByLibrary.simpleMessage("Log In"),
-    "login_title" : MessageLookupByLibrary.simpleMessage("Login"),
-    "login_username" : MessageLookupByLibrary.simpleMessage("username"),
-    "logout_title" : MessageLookupByLibrary.simpleMessage("Log Out"),
-    "message_delete" : MessageLookupByLibrary.simpleMessage(" delete message"),
-    "message_just_now" : MessageLookupByLibrary.simpleMessage("Just now"),
-    "message_logout" : MessageLookupByLibrary.simpleMessage("Log Out"),
-    "message_report" : MessageLookupByLibrary.simpleMessage(" report abuse"),
-    "message_send_button" : MessageLookupByLibrary.simpleMessage("Send message"),
-    "message_title" : MessageLookupByLibrary.simpleMessage("Messages"),
-    "message_under_moderation" : MessageLookupByLibrary.simpleMessage("Message under moderation"),
-    "picture_import_gallery" : MessageLookupByLibrary.simpleMessage("Import from gallery"),
-    "picture_new" : MessageLookupByLibrary.simpleMessage("Take new picture"),
-    "register_EULA_launching_error" : m0,
-    "register_EULA_message" : MessageLookupByLibrary.simpleMessage("I agree with the "),
-    "register_EULA_message_link" : MessageLookupByLibrary.simpleMessage("End of User Agreement, Privacy Policy and Terms & Conditions"),
-    "register_alert_OK_button" : MessageLookupByLibrary.simpleMessage("OK"),
-    "register_alert_title" : MessageLookupByLibrary.simpleMessage("Error"),
-    "register_email" : MessageLookupByLibrary.simpleMessage("email"),
-    "register_error_accept_tc" : MessageLookupByLibrary.simpleMessage("You must accept the T&C"),
-    "register_error_email" : MessageLookupByLibrary.simpleMessage("Must be a valid email address"),
-    "register_error_password" : MessageLookupByLibrary.simpleMessage("Both password must match"),
-    "register_password" : MessageLookupByLibrary.simpleMessage("password"),
-    "register_password_verification" : MessageLookupByLibrary.simpleMessage("password verification"),
-    "register_send_button" : MessageLookupByLibrary.simpleMessage("Register"),
-    "register_title" : MessageLookupByLibrary.simpleMessage("Register"),
-    "register_username" : MessageLookupByLibrary.simpleMessage("username"),
-    "settings_actual_password" : MessageLookupByLibrary.simpleMessage("Actual password"),
-    "settings_change_password_button" : MessageLookupByLibrary.simpleMessage("Change password"),
-    "settings_change_password_title" : MessageLookupByLibrary.simpleMessage("Change my password"),
-    "settings_change_picture" : MessageLookupByLibrary.simpleMessage("Change my profile picture"),
-    "settings_default_message_error_new_password" : MessageLookupByLibrary.simpleMessage("An error has occurred, code: "),
-    "settings_new_password" : MessageLookupByLibrary.simpleMessage("New password"),
-    "settings_title" : MessageLookupByLibrary.simpleMessage("Settings"),
-    "settings_wrong_actual_password" : MessageLookupByLibrary.simpleMessage("Please check your actual password")
-  };
+  static Map<String, Function> _notInlinedMessages(_) => <String, Function>{
+        "channel": MessageLookupByLibrary.simpleMessage("Channels"),
+        "channel_new_button":
+            MessageLookupByLibrary.simpleMessage("     Create"),
+        "channel_new_checkbox":
+            MessageLookupByLibrary.simpleMessage("Public access"),
+        "channel_new_input_hint":
+            MessageLookupByLibrary.simpleMessage("Channel name"),
+        "channel_new_title":
+            MessageLookupByLibrary.simpleMessage("Create new channel"),
+        "login_alert_OK_button": MessageLookupByLibrary.simpleMessage("OK"),
+        "login_error_text": MessageLookupByLibrary.simpleMessage(
+            "Username and/or password do not match."),
+        "login_error_title": MessageLookupByLibrary.simpleMessage(
+            "Are you sure about your credentials?"),
+        "login_password": MessageLookupByLibrary.simpleMessage("password"),
+        "login_send_button": MessageLookupByLibrary.simpleMessage("Log In"),
+        "login_title": MessageLookupByLibrary.simpleMessage("Login"),
+        "login_username": MessageLookupByLibrary.simpleMessage("username"),
+        "logout_title": MessageLookupByLibrary.simpleMessage("Log Out"),
+        "message_delete":
+            MessageLookupByLibrary.simpleMessage(" delete message"),
+        "message_just_now": MessageLookupByLibrary.simpleMessage("Just now"),
+        "message_logout": MessageLookupByLibrary.simpleMessage("Log Out"),
+        "message_report": MessageLookupByLibrary.simpleMessage(" report abuse"),
+        "message_send_button":
+            MessageLookupByLibrary.simpleMessage("Send message"),
+        "message_title": MessageLookupByLibrary.simpleMessage("Messages"),
+        "message_under_moderation":
+            MessageLookupByLibrary.simpleMessage("Message under moderation"),
+        "picture_import_gallery":
+            MessageLookupByLibrary.simpleMessage("Import from gallery"),
+        "picture_new": MessageLookupByLibrary.simpleMessage("Take new picture"),
+        "register_EULA_launching_error": m0,
+        "register_EULA_message":
+            MessageLookupByLibrary.simpleMessage("I agree with the "),
+        "register_EULA_message_link": MessageLookupByLibrary.simpleMessage(
+            "End of User Agreement, Privacy Policy and Terms & Conditions"),
+        "register_alert_OK_button": MessageLookupByLibrary.simpleMessage("OK"),
+        "register_alert_title": MessageLookupByLibrary.simpleMessage("Error"),
+        "register_email": MessageLookupByLibrary.simpleMessage("email"),
+        "register_error_accept_tc":
+            MessageLookupByLibrary.simpleMessage("You must accept the T&C"),
+        "register_error_email": MessageLookupByLibrary.simpleMessage(
+            "Must be a valid email address"),
+        "register_error_password":
+            MessageLookupByLibrary.simpleMessage("Both password must match"),
+        "register_password": MessageLookupByLibrary.simpleMessage("password"),
+        "register_password_verification":
+            MessageLookupByLibrary.simpleMessage("password verification"),
+        "register_send_button":
+            MessageLookupByLibrary.simpleMessage("Register"),
+        "register_title": MessageLookupByLibrary.simpleMessage("Register"),
+        "register_username": MessageLookupByLibrary.simpleMessage("username"),
+        "settings_actual_password":
+            MessageLookupByLibrary.simpleMessage("Actual password"),
+        "settings_change_password_button":
+            MessageLookupByLibrary.simpleMessage("Change password"),
+        "settings_change_password_title":
+            MessageLookupByLibrary.simpleMessage("Change my password"),
+        "settings_change_picture":
+            MessageLookupByLibrary.simpleMessage("Change my profile picture"),
+        "settings_default_message_error_new_password":
+            MessageLookupByLibrary.simpleMessage(
+                "An error has occurred, code: "),
+        "settings_new_password":
+            MessageLookupByLibrary.simpleMessage("New password"),
+        "settings_title": MessageLookupByLibrary.simpleMessage("Settings"),
+        "settings_wrong_actual_password": MessageLookupByLibrary.simpleMessage(
+            "Please check your actual password")
+      };
 }
